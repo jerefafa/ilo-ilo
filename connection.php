e400f7332efd6c0f22b133593efe1a83f469d679
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-19 10:38:30 +0800

    con

--- connection.php
+++ connection.php
@@ -5,4 +5,5 @@
  * Date: 01/11/2018
  * Time: 7:30 AM
  */
-$conn = new mysqli("localhost","root","","iloilo");
+$conn = new mysqli("iloilo-mysqldbserver.mysql.database.azure.com","mysqldbuser@iloilo-mysqldbserver","Pithecus2013","iloilo");
+
