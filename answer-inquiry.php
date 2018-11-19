77e2185b248474f68efebb27c5daf184ac1e0211
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-19 23:42:20 +0800

    asd

--- answer-inquiry.php
+++ answer-inquiry.php
@@ -5,8 +5,7 @@ require "vendor/autoload.php";
 $id = $_POST["id"];
 $stmt = $conn->query("UPDATE `inquiry` SET `replied_by` = '".$_SESSION["user_id"]."' WHERE `id` = '$id'");
 
-$sendGrid = new SendGrid('azure_e5fb4814e91f1eab8b428ad61de261b5@azure.com','Pithecus2013');
-$mail = new SendGrid\Mail('Iloilo Science and Technology University','Response to your question', $_POST["email"], $_POST["reply"]);
-mail($_POST["email"],"Response to your question",$_POST["reply"]);
+$sendGrid = new \SendGrid('azure_e5fb4814e91f1eab8b428ad61de261b5@azure.com','Pithecus2013');
+$mail = new SendGrid\Mail\Mail('Iloilo Science and Technology University','Response to your question', $_POST["email"], $_POST["reply"]);
 $sendGrid->client->mail()->send()->post($mail);
 header("location:inquiry.php");
