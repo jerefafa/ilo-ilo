2fe8734c2b0511c6f6188411261957274323e5fa
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-20 00:56:52 +0800

    bullshit

--- answer-inquiry.php
+++ answer-inquiry.php
@@ -1,41 +1,21 @@
 <?php
 session_start();
 require "connection.php";
-require "vendor/autoload.php";
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 $id = $_POST["id"];
 $stmt = $conn->query("UPDATE `inquiry` SET `replied_by` = '".$_SESSION["user_id"]."' WHERE `id` = '$id'");
+$reply = $_POST["reply"];
+$email = $_POST["email"];
 
-//Server settings
-$mail->SMTPDebug = 1;                                 // Enable verbose debug output
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'jmalicdem45@gmail.com';                 // SMTP username
-$mail->Password = 'Pithecus2013';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-
-//Recipients
-$mail->setFrom('isatu@gmail.com', 'ILOILO SCIENCE AND TECHNOLOGY UNIVERSITY');
-$mail->addAddress($_POST["email"]);     // Add a recipient
-//Content
-$mail->isHTML(true);                                  // Set email format to HTML
-$mail->Subject = 'Your Suffrage Account';
-$message = $_POST["reply"];
-$mail->Body    = $message;
-
-$mail->SMTPOptions = array(
-    'ssl' => array(
-        'verify_peer' => false,
-        'verify_peer_name' => false,
-        'allow_self_signed' => true
+$data = array('reply' => $reply, 'id' => $id, 'email' => $email);
+$url = "http://iloilo.x10host.com/answer-inquiry.php";
+$options = array(
+    'http' => array(
+        'header' => 'Content-type: application/x-www-form-urlencoded\r\n',
+        'method' => 'POST',
+        'content' => http_build_query($data)
     )
 );
-if(!$mail->Send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message has been sent";
-}
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
 //header("location:inquiry.php");
