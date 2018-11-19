29eac5ce5e016769042306eedca45957cb00b4d7
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-19 23:37:36 +0800

    email

--- answer-inquiry.php
+++ answer-inquiry.php
@@ -1,7 +1,12 @@
 <?php
 session_start();
 require "connection.php";
+require "vendor/autoload.php";
 $id = $_POST["id"];
 $stmt = $conn->query("UPDATE `inquiry` SET `replied_by` = '".$_SESSION["user_id"]."' WHERE `id` = '$id'");
+
+$sendGrid = new SendGrid('azure_e5fb4814e91f1eab8b428ad61de261b5@azure.com','Pithecus2013');
+$mail = new SendGrid\Mail('Iloilo Science and Technology University','Response to your question', $_POST["email"], $_POST["reply"]);
 mail($_POST["email"],"Response to your question",$_POST["reply"]);
-header("location:inquiry.php");
+$sendGrid->client->mail()->send()->post($mail);
+header("location:inquiry.php");g
