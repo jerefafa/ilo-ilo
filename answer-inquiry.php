2bcf8af9a93203d571258fa1257a128a77b0f7a8
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-20 11:22:54 +0800

    email

--- answer-inquiry.php
+++ answer-inquiry.php
@@ -1,11 +1,28 @@
+<script src="js/jquery.min.js"></script>
+<script>
+    function sendMail(email, subject, message){
+        $.ajax({
+            url: "http://iloilo.x10host.com/answer-inquiry.php",
+            type: "GET",
+            data: {email: email,
+                subject: subject,
+                reply: message},
+            dataType: "Text",
+            success: function () {
+            }
+        });
+    }
+</script>
 <?php
 require "connection.php";
+session_start();
 if(isset($_POST['rep_user'])){
-        $conn->query("UPDATE `inquiry` SET `replied_by` = '".$_POST["id"]."' WHERE `id`='".$_POST["id"]."'");
+        $conn->query("UPDATE `inquiry` SET `replied_by` = '".$_SESSION["user_id"]."' WHERE `id`='".$_POST["id"]."'");
         $message = $_POST["reply"];
         $email = $_POST["email"];
         $subject = "Response to your question";
         $url = "http://iloilo.x10host.com/answer-inquiry.php?email=$email&subject=$subject&reply=$message";
-        echo "<script>window.open('$url','_blank')</script>";
-//         echo "<script>location.href='inquiry.php'</script>";
+        echo "<script>sendMail('".$email."','".$subject."','".$message."')</script>";
+        echo "<script>location.href='inquiry.php'</script>";
 }
+?>
