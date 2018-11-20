b5389ee896c7d0b7ed6f8f98e25bb7f584f0f0c2
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-20 10:35:06 +0800

    bullshit

--- answer-inquiry.php
+++ answer-inquiry.php
@@ -1,6 +1,4 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
 if(isset($_POST['rep_user'])){
 
         $conn->query("UPDATE `inquiry` SET `replied_by` = '".$_POST["id"]."'");
