51abd95dee0bc1dcd409f30f8c10868941a8c48c
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-20 10:32:20 +0800

    test

--- answer-inquiry.php
+++ answer-inquiry.php
@@ -4,37 +4,11 @@ use PHPMailer\PHPMailer\Exception;
 if(isset($_POST['rep_user'])){
 
         $conn->query("UPDATE `inquiry` SET `replied_by` = '".$_POST["id"]."'");
-//Load Composer's autoloader
-    require 'vendor/autoload.php';
-
-    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-    try {
-        //Server settings
-        $mail->SMTPDebug = 1;                                 // Enable verbose debug output
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'jeffynota@gmail.com';                 // SMTP username
-        $mail->Password = 'Jeffrolan11';                           // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail -> SMTPOptions =array('ssl' => array('verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true ));
-        $mail->Port = 587;                                    // TCP port to connect to
-
-        //Recipients
-        $mail->setFrom('jeffynota@gmail.com', 'Jeff');
-        $mail->addAddress('jeffynota@gmail.com');               // Name is optional
-
-        //Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject = 'ISATU response to your question';
-        $mail->Body    = $_POST["rep_user"];
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-        echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-    }
+        $message = $_POST["rep_user"];
+        $email = $_POST["email"];
+        $subject = "Response to your question";
+        $url = "http://iloilo.x10host.com/answer-inquiry.php?email='$email'&subject='$subject'&reply='$message'";
+        echo $url;
+        echo "<script>window.open('$url','_blank')</script>";
+//    echo "<script>location.href='inquiry.php'</script>";
 }
-
-?>
