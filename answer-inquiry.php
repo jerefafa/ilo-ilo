82dcbdc885a2782252ffe4654011a91f115a0f73
Jeremiah Malicdem <jeremiah.malicdem@ffuf.de>
2018-11-20 10:32:54 +0800

    email

--- answer-inquiry.php
+++ answer-inquiry.php
@@ -7,7 +7,7 @@ if(isset($_POST['rep_user'])){
         $message = $_POST["rep_user"];
         $email = $_POST["email"];
         $subject = "Response to your question";
-        $url = "http://iloilo.x10host.com/answer-inquiry.php?email='$email'&subject='$subject'&reply='$message'";
+        $url = "http://iloilo.x10host.com/answer-inquiry.php?email=$email&subject=$subject&reply=$message";
         echo $url;
         echo "<script>window.open('$url','_blank')</script>";
 //    echo "<script>location.href='inquiry.php'</script>";
