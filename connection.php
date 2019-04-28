88c7ce47ecc485c9ad5a651667979fde060a8576
Jeremiah Malicdem <jmalicdem45@gmail.com>
2019-04-28 22:58:55 +0800

    configure database

--- connection.php
+++ connection.php
@@ -5,6 +5,6 @@
  * Date: 01/11/2018
  * Time: 7:30 AM
  */
-//$conn = new mysqli("iloilo-mysqldbserver.mysql.database.azure.com","mysqldbuser@iloilo-mysqldbserver","Pithecus2013","iloilo");
-$conn = new mysqli("localhost","root","","iloilo");
+$conn = new mysqli("iloilo-db.mysql.database.azure.com","iloilodev@iloilo-db","iloilo123!","iloilo");
+//$conn = new mysqli("localhost","root","","iloilo");
 
